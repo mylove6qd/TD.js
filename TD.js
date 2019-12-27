f3e1096e718e30567e7ecae6c8917c136eb91eda
lichenxi <tian1129>
2019-12-27 11:34:32 +0800

    点击事件 基础建设

--- TD.js
+++ TD.js
@@ -100,14 +100,11 @@ class TD {
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
         //-----------------------------------------------------------------属性-------------------------------------------------------------------------
         //-----------------------------------------------------------------初始化-------------------------------------------------------------------------
-        //初始化相机
         TD.prototype._initCamera(this);
-        //初始化渲染器
         TD.prototype._initRenderer(this);
-        //初始化性能工具
         TD.prototype._initStats(this);
-        //初始化视角工具
         TD.prototype._initControls(this);
+        TD.prototype._addMouseListener(this);
         //默认是测试
         this.setTest(true);
         //窗口自动变化
@@ -117,8 +114,24 @@ class TD {
         //-----------------------------------------------------------------初始化-------------------------------------------------------------------------
     }
 }
-
 //-----------------------------------------------------------------其他方法-------------------------------------------------------------------------
+//添加Object3D的事件监听
+TD.prototype._addMouseListener = function (obj) {
+    obj.container.addEventListener("mousedown", (event) => {
+        let mouse = new THREE.Vector2();
+        let raycaster = new THREE.Raycaster();
+        // 计算鼠标点击位置转换到3D场景后的位置
+        mouse.x = (event.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
+        mouse.y = -(event.clientY / this.renderer.domElement.clientHeight) * 2 + 1;
+        // 由当前相机（视线位置）像点击位置发射线
+        raycaster.setFromCamera(mouse, this.camera);
+        let intersects = raycaster.intersectObjects(this.scene.children, true);
+        if (intersects.length > 0) {
+            // 拿到射线第一个照射到的物体
+            console.log(intersects[0].object);
+        }
+    });
+};
 // 窗口大小变动时调用
 TD.prototype._handleWindowResize = function () {
     // 更新渲染器的高度和宽度以及相机的纵横比
@@ -128,12 +141,14 @@ TD.prototype._handleWindowResize = function () {
     // this.camera.aspect = this._WIDTH / this._HEIGHT;
     // this.camera.updateProjectionMatrix();
 };
+//初始化性能工具
 TD.prototype._initStats = function (obj) {
     // 将性能监控屏区显示在左上角
     obj.stats.domElement.style.position = 'absolute';
     obj.stats.domElement.style.bottom = '0px';
     obj.stats.domElement.style.zIndex = 100;
 };
+//初始化视角工具
 TD.prototype._initControls = function (obj) {
     obj.controls.maxPolarAngle = Math.PI * 0.5; //最大俯视角
     obj.controls.minDistance = 1; //最小相机移动距离
@@ -142,6 +157,7 @@ TD.prototype._initControls = function (obj) {
     obj.controls.dampingFactor = 0.25; //惯性滑动
     // obj.controls.autoRotate = true;
 };
+//初始化相机
 TD.prototype._initCamera = function (obj) {
     obj.camera.position.x = -10;
     obj.camera.position.y = 10;
@@ -149,6 +165,7 @@ TD.prototype._initCamera = function (obj) {
     //默认相机看向场景中心
     obj.camera.lookAt(obj.scene.position);
 };
+//初始化渲染器
 TD.prototype._initRenderer = function (obj) {
     obj.renderer.setSize(obj._WIDTH, obj._HEIGHT);
     //开启阴影效果需要渲染器支持，如果使用的WebGLRender 渲染器 需要开启渲染器
@@ -161,5 +178,36 @@ TD.prototype._initRenderer = function (obj) {
 //随机数
 TD.prototype.Trand = function (min, max) {
     return Math.random() * (max - min) + min;
+};
+
+//为所有Object3D对象添加事件
+//点击事件click
+THREE.Object3D.prototype.click = function (fn,obj) {
+    this._click = fn || undefined;
+    this._clickObj = obj || this;
+};
+//点击穿透事件clickThrough
+THREE.Object3D.prototype.clickThrough = function (fn,obj) {
+    this._clickThrough = fn || undefined;
+    this._clickThroughObj = obj || this;
+};
+//双击事件dblclick
+THREE.Object3D.prototype.dblclick = function (fn,obj) {
+    this._dblclick = fn || undefined;
+    this._dblclickObj = obj || this;
+};
+//双击穿透事件dblclickThrough
+THREE.Object3D.prototype.dblclickThrough = function (fn,obj) {
+    this._dblclickThrough = fn || undefined;
+    this._dblclickThroughObj = obj || this;
+};
+//hover事件   hover=mouseenter + mouseleave
+THREE.Object3D.prototype.hover = function (fn,obj) {
+    this._hover = fn || undefined;
+    this._hoverObj = obj || this;
+};
+THREE.Object3D.prototype.hoverThrough = function (fn,obj) {
+    this._hoverThrough = fn || undefined;
+    this._hoverThroughObj = obj || this;
 };
 //-----------------------------------------------------------------其他方法-------------------------------------------------------------------------
